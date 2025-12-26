17689b57e7285e772a740fa29868f4c291c94e26
dmswl2515 <dmswl2515@gmail.com>
2025-12-26 16:05:37 +0900

    코딩테스트 연습용 파일 추가

---
+++ Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("코딩테스트 연습 완료!");
+        System.out.println("GitHub Fork -> CLone -> PR 성공!");
+    }
+}
